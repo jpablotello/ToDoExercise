e07892889976261a2d8a87b3f9e6b9a81882e3fe
VASALLO,Diego <7@7.com>
2019-07-31 10:05:29 -0300

    Primer commit

--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TodoService } from 'src/app/services/todo.service';
+import { Lista } from '../../models/lista.model';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +9,7 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  constructor(public todoservice: TodoService) {
 
+  }
 }
